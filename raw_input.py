c28d4dc9d0c015cfdd7e7beb9da9caf8d3c6f6d8
gaoyanping <tiandi_shaou@126.com>
2014-03-03 18:56:14 +0800

    updatge

--- raw_input.py
+++ raw_input.py
@@ -4,6 +4,7 @@
 a = 50
 b = 60
 c = raw_input()
+f = "I"
 #rwa_input()默认的是字符
 print "c is %s" %c
 #将int型转换成字符型
@@ -12,3 +13,8 @@ print str(a)+str(b)+c
 #可以将raw_input()转换成int型的
 d = int(raw_input())
 print a+b+d
+
+#加上输入时候的提示
+e = raw_input("Please inter the word:")
+
+print f+e
