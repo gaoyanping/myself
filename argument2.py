ad175be0d7dbb33e15b1422be995557f5616c4d4
gaoyanping <tiandi_shaou@126.com>
2014-03-04 18:25:55 +0800

    renew

---
+++ argument2.py
@@ -0,0 +1,13 @@
+# -*- coding=utf-8 -*-
+
+from sys import argv
+
+script,name = argv
+
+print "I love %s." %name
+
+pop = ">>>>>>>"
+
+name_2=raw_input(pop)
+
+print "I love %s and %s too." %(name,name_2)
--- raw_input.py
+++ raw_input.py
@@ -3,18 +3,18 @@
 
 a = 50
 b = 60
-c = raw_input()
-f = "I"
+c = raw_input("input number:")
+f = "I Love "
 #rwa_input()默认的是字符
 print "c is %s" %c
 #将int型转换成字符型
 print str(a)+str(b)+c
 
 #可以将raw_input()转换成int型的
-d = int(raw_input())
+d = int(raw_input("input number2:"))
 print a+b+d
 
 #加上输入时候的提示
-e = raw_input("Please inter the word:")
+e = raw_input("I love who?")
 
 print f+e
