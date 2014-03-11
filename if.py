d9ac4f28ca430d508c845f11bfb32aa8e472efaa
gaoyanping <tiandi_shaou@126.com>
2014-03-11 16:07:34 +0800

    if

---
+++ if.py
@@ -0,0 +1,12 @@
+#-*- coding=utf-8 -*-
+
+people = 30
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "people < cats;"
+elif people > cats:
+	print "people > cats;"
+else:
+	print "people = cats;"
